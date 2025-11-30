3b8cb81de23de9ca8fc3604240f819f397c831b9
Olivia Hill <hill.ol@northeastern.edu>
2025-11-29 19:21:30 -0500

    fixing the coverage

--- coopscout-frontend/src/pages/Profile.tsx
+++ coopscout-frontend/src/pages/Profile.tsx
@@ -3,7 +3,7 @@ import './Profile.css'
 function Profile() {
     console.log("Profile component rendered!");
     return (
-        <div className="App" style={{backgroundColor: 'lightblue', minHeight: '100vh'}}>
+        <div className="profile-page" style={{backgroundColor: 'lightblue'}}>
             <h1 className="title">Profile Page</h1>
             <p className="paragraph">This is your profile!</p>
         </div>
